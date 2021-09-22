c04a16fd4bdf30734b8adf26f2d333dcd3356090
June <williamreji66@gmail.com>
2021-09-22 11:57:53 +0530

    Create FCFS.c

---
+++ FCFS.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//typedef unsigned int size_t;
+const size_t ArraySize = 10;
+size_t Waiting_time = 0;
+size_t TurnAround_time = 0;
+
+
+struct Proc
+{
+	size_t BurstTime;
+
+};
+
+void Populate(struct Proc* Array)
+{
+	for(int i  = 0; i<ArraySize;i++)
+	{
+		Array[i].BurstTime = (size_t) random();
+	}
+};
+
+void OutPut(struct Proc* Array)
+{
+
+	printf("Process \t TurnaroundTime \t Bursttime \t Waiting \t \n");
+	for(int i = 0;i<ArraySize;i++)
+	{
+		TurnAround_time = Array->BurstTime + Waiting_time;
+		printf("P[%d] \t\t %lu \t\t %lu \t\t %lu \n",i,TurnAround_time,Array->BurstTime,Waiting_time);   
+		Waiting_time = Waiting_time + Array->BurstTime;
+	}
+	
+}
+
+int main()
+{
+	struct Proc Array[ArraySize];
+	Populate(Array);//Array decay
+	OutPut(Array);
+}
